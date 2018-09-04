e80a9438e1838d73b3cc02d6b0edcfee4bd0f557
Carlos Josué Morales R <carlosjosum@hotmail.com>
2018-09-04 01:39:56 -0600

    Login and Register methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   resources :community_members
   resources :communities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace 'api' do
+    post 'users/login', to: 'users#login'
+    post 'users/register', to: 'users#register'
+  end
 end
