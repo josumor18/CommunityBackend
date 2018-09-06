ac737c9502dbe17c4f23a84254e3d331569f3979
Carlos Josué Morales R <carlosjosum@hotmail.com>
2018-09-05 20:10:46 -0600

    get communities

--- db/migrate/20180906020540_create_communities.rb
+++ db/migrate/20180906020540_create_communities.rb
@@ -3,11 +3,11 @@ class CreateCommunities < ActiveRecord::Migration[5.0]
     create_table :communities do |t|
       t.string :name
       t.string :description
-      t.string :rules
+      t.string :rules, :array => true
       t.boolean :isSubcommunity
       t.string :photo
       t.string :photo_thumbnail
-      t.integer :sub_communities
+      t.integer :sub_communities, :array => true
 
       t.timestamps
     end
