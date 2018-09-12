e9eed6f80ee9042e22aeed85e9f2ad94288c0e9a
Carlos Josué Morales R <carlosjosum@hotmail.com>
2018-09-12 02:53:14 -0600

    Accept/Refuse requests

--- app/controllers/api/requests_controller.rb
+++ app/controllers/api/requests_controller.rb
@@ -9,6 +9,7 @@ module Api
             if(user.auth_token == token)
                 reqs = Request.where(id_community: params[:id_community])
                 users_req = []
+                seens = []
 
                 reqs.each do |req|
                     user = User.where(id: req.id_user).first
@@ -16,6 +17,10 @@ module Api
                     if(user)
                         users_req.push(user)
                     end
+                    
+                    seens.push(req.seen)
+
+                    req.update(:seen=>true)
                 end
 
                 #---------- Cambiar authentication token ----------
@@ -24,7 +29,51 @@ module Api
                 user.auth_token = (0...20).map { o[rand(o.length)] }.join
                 user.save
                 #--------------------------------------------------
-                render json: { status: 'SUCCESS', message: 'Solicitudes obtenidas', users: users_req, auth_token: user.auth_token }, status: :ok
+                render json: { status: 'SUCCESS', message: 'Solicitudes obtenidas', users: users_req, seens: seens, auth_token: user.auth_token }, status: :ok
+            else
+                render json: { status: 'INVALID', message: 'Token invalido'}, status: :unauthorized
+            end
+        end
+
+        #POST accept request
+        def accept
+            user = User.where(id: params[:id]).first
+            token = params[:auth_token]
+            if(user.auth_token == token)
+                mem = CommunityMember.new(id_community: params[:id_community], id_user: params[:id_user], isAdmin: false)
+
+                if(mem.save)
+                    Request.where(id_community: params[:id_community]),where(id_user: params[:id_user]).destroy_all
+
+                    #---------- Cambiar authentication token ----------
+                    user.auth_token = nil
+                    o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
+                    user.auth_token = (0...20).map { o[rand(o.length)] }.join
+                    user.save
+                    #--------------------------------------------------
+                    render json: { status: 'SUCCESS', message: 'Usuario agregado', auth_token: user.auth_token }, status: :ok
+                else
+                    render json: { status: 'SUCCESS', message: 'Usuario NO agregado', auth_token: user.auth_token }, status: :unauthorized
+                end
+                
+                
+            else
+                render json: { status: 'INVALID', message: 'Token invalido'}, status: :unauthorized
+            end
+        end
+
+        #PUT seen request
+        def put_seen
+            user = User.where(id: params[:id]).first
+            token = params[:auth_token]
+            if(user.auth_token == token)
+                reqs = Request.where(id_community: params[:id_community])
+
+                reqs.each do |req|
+                    req.update(:seen=>true)
+                end
+
+                render json: { status: 'SUCCESS', message: 'Solicitudes actualizadas' }, status: :ok
             else
                 render json: { status: 'INVALID', message: 'Token invalido'}, status: :unauthorized
             end
