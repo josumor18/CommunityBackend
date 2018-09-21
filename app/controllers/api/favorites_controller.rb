5ba00260a3a7acf04691d12cc33d4b634f7817fd
Diego Armando Guzmán Villalta <33551960+Diego-Guzman@users.noreply.github.com>
2018-09-21 08:11:39 -0600

    auth get_newsFavorites

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -15,15 +15,30 @@ module Api
     end
 
     def get_newsFavorites
-      res = []
-      favs = Favorite.where(id_user: params[:id])
 
-      favs.each do |item|
-          post = New.where(id: item.id_news).first
-          res.push(post)
+      user = User.where(id: params[:id]).first
+      pass = params[:auth_token]
+      if (user && user.auth_token == pass)
+        res = []
+        favs = Favorite.where(id_user: params[:id])
+
+        favs.each do |item|
+            post = New.where(id: item.id_news).first
+            res.push(post)
+        end
+        
+        #---------- Cambiar authentication token ----------
+        user.auth_token = nil
+        o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
+        user.auth_token = (0...20).map { o[rand(o.length)] }.join
+        user.save
+        #--------------------------------------------------
+
+        render json: { status: 'SUCCESS', message: 'Difusiones obtenidas de favoritos', news: res, auth_token: user.auth_token }, status: :ok
+      else
+        render json: { status: 'INVALID', message: 'Token invalido'}, status: :unauthorized
       end
-      
-      render json: { status: 'SUCCESS', message: 'Difusiones obtenidas de favoritos', news: res}, status: :ok
+
     end
 
  
