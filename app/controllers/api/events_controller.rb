070df0e49f558418126a3fe1c5c8b1529406ab89
Diego Armando Guzmán Villalta <33551960+Diego-Guzman@users.noreply.github.com>
2018-10-01 16:29:50 -0600

    error fixed

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -124,7 +124,7 @@ module Api
                 members = CommunityMember.where(id_community: idCom)
                 members.each do |item|
                     notif = Notification.new(idUser: item.id_user, idContent: event.id, isNews: false, isReports: false, isEvents: true, titleContent:  event.title, seen: false, photo: event.photo)
-                    notif.save,
+                    notif.save
                 end
 
 
