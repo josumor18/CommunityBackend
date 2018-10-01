44dd8b51ee456eae99810ca2ff10592d7172ff6f
Diego Armando Guzmán Villalta <33551960+Diego-Guzman@users.noreply.github.com>
2018-10-01 16:42:10 -0600

    .

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -19,7 +19,7 @@ module Api
 
                     #notifications
                     isApproved = params[:approved]
-                    if(isApproved)
+                    if(isApproved == true)
                         members = CommunityMember.where(id_community: params[:idCommunity])
                         members.each do |item|
                             notif = Notification.new(idUser: item.id_user, idContent: eve.id, isNews: false, isReports: false, isEvents: true, titleContent:  params[:title], seen: false, photo: params[:photo])
